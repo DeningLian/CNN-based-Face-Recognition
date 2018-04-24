b73d1bb62119a239f1254d5b2360995465368909
zmx976508106 <976508106@qq.com>
2018-04-24 15:57:24 +0800

    方向识别完成

--- src/ann.py
+++ src/ann.py
@@ -9,7 +9,7 @@ from numpy import shape
 # 加载数据
 # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 img_width = 28
-data, num = read_data_sets(img_width=img_width)
+data, num = read_data_sets(path='../data/faces_dir', img_width=img_width)
 
 # x 不是一个特定的值，而是一个占位符placeholder
 # 我们在TensorFlow运行计算时输入这个值，也就是说，x 是输入值
@@ -38,14 +38,13 @@ y = tf.nn.softmax(tf.matmul(x,W) + b)
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 
 # 在这里，我们要求TensorFlow用梯度下降算法（gradient descent algorithm）以0.01的学习速率最小化交叉熵。
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
 
 # 现在，我们已经设置好了我们的模型。在运行计算之前，我们需要添加一个操作来初始化我们创建的变量：
-init = tf.initialize_all_variables()
 
 # 现在我们可以在一个Session里面启动我们的模型，并且初始化变量：
 sess = tf.Session()
-sess.run(init)
+sess.run(tf.global_variables_initializer())
 
 # tf.argmax 是一个非常有用的函数，它能给出某个tensor对象在某一维上的其数据最大值所在的索引值。
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
@@ -54,17 +53,10 @@ correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 # 然后开始训练模型，这里我们让模型循环训练1000次！
-test = False
 validate_data = get_validate_data()
-for i in range(1000):
+for i in range(500):
 	for batch in train.get_batches(10):
 		# batch_xs, batch_ys = mnist.train.next_batch(10)
-		if test:
-			print(type(batch[0]), type(batch[1]))
-			print(shape(batch[0]), shape(batch[1]))
-			print(batch[0])
-			print(batch[1])
-			abc = eval(input('shit'))
 		sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
 	if i % 5 == 0:
 		# correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
@@ -81,6 +73,5 @@ test_data = get_test_data()
 with sess.as_default():
 	print(accuracy.eval(feed_dict={x: test_data[0], y_: test_data[1]}))
 # print type(sess.run(tf.constant([1,2,3])))
-
 
 
